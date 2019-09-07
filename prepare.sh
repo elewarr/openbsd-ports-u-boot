db91f66fd2c6555cebec8cf63fc83dbceaa7ffdc
Krystian Lewandowski <k.lewandowski@me.com>
2019-09-07 03:26:33 +0200

    different patching mechanism, battery and thermal zones

--- prepare.sh
+++ prepare.sh
@@ -1,27 +1,52 @@
 #!/usr/bin/env ksh
 
-FLAVOR=aarch64
+export FLAVOR=aarch64
 UBOOT_OVERLAYS_PATH=$(pwd)/overlays
 
 cd /usr/ports/sysutils/u-boot
 WRKDIST=$(make show=WRKDIST)
+echo ${WRKDIST}
+make uninstall
 make clean=all
 make patch
 cd -
 
-WRKDIST_OVERLAYS_PATH=${WRKDIST}/arch/arm/dts/overlays/arm64
+WRKDIST_DTS_PATH="${WRKDIST}/arch/arm/dts/"
+WRKDIST_OVERLAYS_PATH="${WRKDIST}/arch/arm/dts/overlays/"
 
 if [ -z "${WRKDIST}" ]; then
-	echo "no WRKDIST"
-	exit 1
+    echo "no WRKDIST"
+    exit 1
 fi
 
-cd ${WRKDIST}
+cd "${WRKDIST}"
 
-rm -rf ${WRKDIST_OVERLAYS_PATH}
-mkdir -p ${WRKDIST_OVERLAYS_PATH}
+rm -rf "${WRKDIST_OVERLAYS_PATH}"
+mkdir -p "${WRKDIST_OVERLAYS_PATH}"
 
-for F in $(find ${UBOOT_OVERLAYS_PATH} -name "sun50i-a64*.dtso"); do
-	tail -n+3 ${F} > ${WRKDIST_OVERLAYS_PATH}/$(basename ${F}.updated)
-	echo "#include <overlays/arm64/$(basename ${F}).updated>" >> ${WRKDIST}/arch/arm/dts/sun50i-a64.dtsi
+for P in $(find "${UBOOT_OVERLAYS_PATH}" -name "*.dtso"); do
+    F=$(basename "${P}")
+    SUFFIX=${F##*-}
+    TARGET_DTS=${WRKDIST_DTS_PATH}${F%-"${SUFFIX}"}.dts
+    TARGET_DTSI=${WRKDIST_DTS_PATH}${F%-"${SUFFIX}"}.dtsi
+    TARGET=""
+
+    if [ -f "${TARGET_DTS}" ]; then
+        TARGET="${TARGET_DTS}"
+    elif [ -f "${TARGET_DTSI}" ]; then
+        TARGET="${TARGET_DTSI}"
+    fi
+
+    if [ ! -z "${TARGET}" ]; then
+        cp "${P}" "${WRKDIST_OVERLAYS_PATH}/${F}"
+        echo -n "Adding ${F} include to $(basename ${TARGET})... "
+        echo "#include <overlays/${F}>" >> ${TARGET}
+        echo "OK"
+    else
+        echo "Couldn't fid target for ${P} overlay!"
+    fi
 done
+
+cd /usr/ports/sysutils/u-boot
+make install
+cd -
